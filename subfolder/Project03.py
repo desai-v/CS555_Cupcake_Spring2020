36b313a0736f84a5919e947ac6cc31aacd001eb8
vidushidesai5 <60893700+vidushidesai5@users.noreply.github.com>
2020-02-16 20:09:00 -0500

    Add files via upload

---
+++ subfolder/Project03.py
@@ -0,0 +1,65 @@
+from subfolder.lineValidity import isvalid
+from subfolder.parseObjects import inddetails, famdetails
+from subfolder.outputDisplay import outputtable
+
+
+def main() -> None:
+
+    f = open("Project01_Diksha_Pancholi.ged", "r")
+
+    obj = None 
+    currtag = None 
+    indi = list()  
+    fam = list()  
+
+    for line in f:
+        elems = line.split()
+        v = isvalid(elems)
+        if v is None:
+            continue
+
+        
+
+        if v[0] is '0' and (v[1] in ('FAM', 'INDI')):
+            if currtag is not None:
+                if 'INDI' in obj:
+                    indi.append(obj)
+                else:
+                    fam.append(obj)
+                obj = None
+            
+            currtag = v[1]
+
+        elif v[0] is '0':
+            # NOTE, TRLR, HEAD
+            continue
+
+        
+        if currtag == 'FAM':
+            obj = famdetails(obj, v)
+        elif currtag == 'INDI':
+            obj = inddetails(obj, v)
+        elif obj is None: 
+            currtag = None
+    
+
+    
+    if 'INDI' in obj:
+        indi.append(obj)
+    else:
+        fam.append(obj)
+
+    f.close()
+
+    
+    indi = sorted(indi, key=lambda i: i["INDI"])
+    fam = sorted(fam, key=lambda i: i["FAM"])
+
+    
+    outputtable(indi, fam)
+
+   
+
+
+if __name__ == "__main__":
+    main()
